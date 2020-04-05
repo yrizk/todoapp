4a26689b1566477fedb445d6d4c44369079f35a9
yrizk <yohan.rizk@gmail.com>
2020-04-05 13:06:43 -0700

    figured out the deletion flow. also the create task fully works by setting the right fields on the dom and by sending down the right properties

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 import sys
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, redirect, url_for, request, jsonify, abort
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yrizk@localhost:5432/yrizk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yrizk@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
-        return f'<Todo {self.description}>'
+        return f'<Todo {self.id} {self.description} {self.completed}>'
 
 @app.route('/')
 def index():
-    data = Todo.query.all()
+    data = Todo.query.order_by('id').all()
     return render_template('index.html', data=data)
 
 @app.route('/todos/create', methods=['POST'])
@@ -31,6 +36,8 @@ def create_todo():
     db.session.add(todo)
     db.session.commit()
     body['description'] = todo.description
+    body['id'] = todo.id
+    body['completed'] = todo.completed
   except:
     error = True
     db.session.rollback()
@@ -41,3 +48,28 @@ def create_todo():
     abort (400)
   else:
     return jsonify(body)
+
+
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
+
+@app.route('/todos/<todo_id>/update', methods=['POST'])
+def update_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        t = Todo.query.get(todo_id)
+        t.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
